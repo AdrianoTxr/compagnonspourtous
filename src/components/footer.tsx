f52dd06e2aa3ce24cf93bb1a7f058e94f28e40d4
Adriano Teixeira <63314477+AdrianoTxr@users.noreply.github.com>
2024-05-16 02:02:29 +0200

    fixed responsive things like socials buttons not aligned with the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className="border-t bg-white">
-            <div className="footer px-10 py-4 text-base-content border-base-300 max-w-screen-xl mx-auto">
+            <div className="footer px-10 py-4 text-base-content border-base-300 max-w-screen-xl mx-auto grid-flow-col">
                 <aside className="items-center grid-flow-col">
                     <Image src={'/logo.png'} alt={'Logo Compagnons pour Tous'} width={50} height={50}/>
                     <p><span className={'font-bold'}>Compagnons pour Tous.</span><br/>Association sous loi 1901</p>
                 </aside>
-                <nav className="md:place-self-center md:justify-self-end">
+                <nav className="place-self-center justify-self-end">
                     <div className="grid grid-flow-col gap-4">
                         <a href="https://instagram.com/compagnonspour.tous"><i
                             className="fa-brands fa-instagram text-xl"></i></a>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,19 +43,15 @@ export default function Header() {
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <Link href="/public" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                className="h-8 w-auto"
-                                src="/logo.png"
-                                alt=""
-                            />
+                            <span className="sr-only">Compagnons pour Tous</span>
+                            <Image src={'/logo.png'} alt={'Logo Compagnons pour Tous'} width={50} height={50} className={"h-8 w-auto"} />
                         </Link>
                         <button
                             type="button"
                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
                             onClick={() => setMobileMenuOpen(false)}
                         >
-                            <span className="sr-only">Close menu</span>
+                            <span className="sr-only">Fermer le menu</span>
                             <i className="fa-solid fa-xmark" aria-hidden="true"></i>
                         </button>
                     </div>
