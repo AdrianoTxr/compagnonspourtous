f377d1349cefa7942727106fa6129e9b9eef7d0c
Adriano Teixeira <63314477+AdrianoTxr@users.noreply.github.com>
2024-05-22 09:03:56 +0200

    Update team.tsx

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -1,27 +1,27 @@
 const people = [
     {
-        name: 'John Doe1',
+        name: 'Alexandre',
         role: 'Président',
         imageUrl:
-            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            '/images/alexandre.JPEG',
     },
     {
-        name: 'John Doe2',
+        name: 'Emir',
         role: 'Trésorier',
         imageUrl:
-            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            '/images/emir.JPEG',
     },
     {
-        name: 'John Doe3',
+        name: 'Adriano',
         role: 'Secrétaire',
         imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            '/images/adriano.JPEG',
     },
     {
-        name: 'Jane Doe',
+        name: 'Maya',
         role: 'Chargée de communcation',
         imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            '/images/maya.JPEG',
     },
 ]
 
